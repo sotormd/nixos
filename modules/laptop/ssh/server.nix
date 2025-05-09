ba32d848e9cc2abaa768c8219675bf99b6559a0f
vrm <sotormd@proton.me>
2025-05-09 12:26:45 +0530

    laptop: add vars.network.server.enabled option to enable/disable server features

--- modules/laptop/ssh/server.nix
+++ modules/laptop/ssh/server.nix
@@ -1,13 +1,17 @@
 { home-manager, vars, ... }:
 
 {
-  home-manager.users."${vars.user.name}" = {
-    programs.ssh.matchBlocks.server = {
-      hostname = vars.network.server.ip;
-      port = vars.network.server.ssh.port;
-      user = vars.user.name;
-      identityFile = "/home/${vars.user.name}/.ssh/${vars.network.server.ssh.keyfile}";
-      identitiesOnly = true;
-    };
-  };
+  home-manager.users."${vars.user.name}" =
+    if (vars.network.server.enabled == true) then
+      {
+        programs.ssh.matchBlocks.server = {
+          hostname = vars.network.server.ip;
+          port = vars.network.server.ssh.port;
+          user = vars.user.name;
+          identityFile = "/home/${vars.user.name}/.ssh/${vars.network.server.ssh.keyfile}";
+          identitiesOnly = true;
+        };
+      }
+    else
+      { };
 }
