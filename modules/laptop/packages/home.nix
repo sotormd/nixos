9a134074896c2a02fd58c52ff1de2c8317e054b7
vrm <sotormd@proton.me>
2025-10-12 18:47:51 +0530

    refactor(common,laptop): clean packages list

--- modules/laptop/packages/home.nix
+++ modules/laptop/packages/home.nix
@@ -7,26 +7,14 @@
       # audio visualizer
       cava
 
-      # system information tool
-      fastfetch
-
       # compression
       file-roller
-
-      # resource monitor
-      htop
 
       # notifications
       libnotify
 
-      # text editor
-      nano
-
       # wayland image viewer
       swayimg
-
-      # list contents of directories in a tree-like format
-      tree
 
       # wayland clipboard
       wl-clipboard
