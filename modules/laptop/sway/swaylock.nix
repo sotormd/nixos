274220ae97e738d20aa4e4cb0ec67c8873f8731b
vrm <sotormd@proton.me>
2025-10-15 21:09:58 +0530

    style(laptop): cleaner swaylock indicator

--- modules/laptop/sway/swaylock.nix
+++ modules/laptop/sway/swaylock.nix
@@ -20,20 +20,23 @@
       # colors
       bs-hl-color = "#${colors.yellow}";
       inside-color = "#${colors.bg0}00";
-      inside-clear-color = "#${colors.yellow}";
-      inside-ver-color = "#${colors.blue2}";
-      inside-wrong-color = "#${colors.red}";
+      inside-clear-color = "#${colors.yellow}00";
+      inside-ver-color = "#${colors.blue2}00";
+      inside-wrong-color = "#${colors.red}00";
       key-hl-color = "#${colors.blue2}";
-      ring-color = "#${colors.bg0}";
+      ring-color = "#${colors.bg0}00";
       ring-clear-color = "#${colors.yellow}";
       ring-ver-color = "#${colors.blue2}";
       ring-wrong-color = "#${colors.red}";
-      text-color = "#${colors.bg0}";
-      text-caps-lock-color = "#${colors.yellow}";
+      text-color = "#${colors.bg0}00";
+      text-ver-color = "#${colors.bg0}00";
+      text-clear-color = "#${colors.bg0}00";
+      text-wrong-color = "#${colors.bg0}00";
+      text-caps-lock-color = "#${colors.yellow}00";
 
       # indicator options
       indicator-radius = 200;
-      indicator-thickness = 15;
+      indicator-thickness = 7;
 
       # line options
       line-uses-inside = true;
