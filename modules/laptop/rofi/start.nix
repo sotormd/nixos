d3a54ff749d1e9d0c816c365e66961a4af6f2d86
vrm <sotormd@proton.me>
2025-10-19 15:24:06 +0530

    feat(laptop): workspace switcher with rofi

--- modules/laptop/rofi/start.nix
+++ modules/laptop/rofi/start.nix
@@ -1,7 +1,14 @@
-{ vars, ... }:
+{ pkgs, vars, ... }:
 
 {
   home-manager.users.${vars.user.name} = {
-    wayland.windowManager.sway.config.keybindings."Mod4+d" = ''exec rofi -show run'';
+    wayland.windowManager.sway.config.keybindings = {
+      "Mod4+d" = ''
+        exec rofi -show run
+      '';
+      "Mod4+g" = ''
+        exec ${pkgs.swayfx}/bin/swaymsg workspace $(${pkgs.swayfx}/bin/swaymsg -t get_workspaces -r | ${pkgs.jq}/bin/jq -r '.[].name' | rofi -dmenu -p "")
+      '';
+    };
   };
 }
