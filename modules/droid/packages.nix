4da6ed8755ec84eddca59f2e306495e6fa9e23f3
vrm <sotormd@proton.me>
2025-10-19 14:28:43 +0530

    feat(droid): add nix-on-droid configuration

---
+++ modules/droid/packages.nix
@@ -0,0 +1,43 @@
+{ pkgs, ... }:
+
+{
+  # packages to appear in the system environment
+  environment.packages = with pkgs; [
+    micro
+    procps
+    killall
+    diffutils
+    utillinux
+    tzdata
+    hostname
+    man
+    gnugrep
+    gnupg
+    gnused
+    gnutar
+    bzip2
+    gzip
+    xz
+    zip
+    unzip
+    fastfetch
+    stack
+    cabal-install
+    ghc
+    cargo
+    rustc
+    go
+    openssh
+    git
+    python3
+    less
+    curl
+    wget
+    findutils
+    coreutils
+    less
+    bashInteractive
+    htop
+    ncurses
+  ];
+}
