0af30b01b08e100b68d476b1d61ea6ccb850a3c9
vrm <sotormd@proton.me>
2025-10-19 15:48:36 +0530

    fix(droid): fix aliases

--- modules/droid/home.nix
+++ modules/droid/home.nix
@@ -4,6 +4,8 @@
   home.sessionVariables.NIXOS_ROLE = "droid";
   home.sessionVariables.PS1 = ''\n\[\033[1;32m\]nix \w \$\[\033[0m\] '';
 
-  home.shellAliases.switch = "nix-on-droid switch --flake github:sotormd/nixos";
-  home.shellAliases.purge = "nix-collect-garbage --delete-old";
+  home.file.".bashrc".text = ''
+    alias switch="nix-on-droid switch --flake github:sotormd/nixos"
+    alias purge="nix-collect-garbage --delete-old"
+  '';
 }
