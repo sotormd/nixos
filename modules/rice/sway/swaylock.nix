3c0a9954597dfdc9931dfb63eaf4ce3484da69b2
vrm <sotormd@proton.me>
2025-10-25 09:51:57 +0530

    style(rice): cleaner swaylock

--- modules/rice/sway/swaylock.nix
+++ modules/rice/sway/swaylock.nix
@@ -14,12 +14,12 @@
     font-size = 16;
 
     # colors
-    bs-hl-color = "#${colors.yellow}";
+    bs-hl-color = "#${colors.yellow}00";
     inside-color = "#${colors.bg0}00";
     inside-clear-color = "#${colors.yellow}00";
     inside-ver-color = "#${colors.blue2}00";
     inside-wrong-color = "#${colors.red}00";
-    key-hl-color = "#${colors.blue2}";
+    key-hl-color = "#${colors.blue2}00";
     ring-color = "#${colors.bg0}00";
     ring-clear-color = "#${colors.yellow}";
     ring-ver-color = "#${colors.blue2}";
@@ -29,6 +29,19 @@
     text-clear-color = "#${colors.bg0}00";
     text-wrong-color = "#${colors.bg0}00";
     text-caps-lock-color = "#${colors.yellow}00";
+    inside-caps-lock-color = "#00000000";
+    ring-caps-lock-color = "#00000000";
+    line-color = "#00000000";
+    line-clear-color = "#00000000";
+    line-ver-color = "#00000000";
+    line-wrong-color = "#00000000";
+    line-caps-lock-color = "#00000000";
+    separator-color = "#00000000";
+    caps-lock-bs-hl-color = "#00000000";
+    caps-lock-key-hl-color = "#00000000";
+    layout-bg-color = "#00000000";
+    layout-border-color = "#00000000";
+    layout-text-color = "#00000000";
 
     # indicator options
     indicator-radius = 200;
