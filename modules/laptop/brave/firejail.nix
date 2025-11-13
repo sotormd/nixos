12e51806a82fb5d3b1533af2f39745039d97e890
vrm <sotormd@proton.me>
2025-11-13 20:40:30 +0530

    feat(laptop): add brave webapps

--- modules/laptop/brave/firejail.nix
+++ modules/laptop/brave/firejail.nix
@@ -8,14 +8,37 @@
 
 let
   package = import ./package.nix { inherit pkgs config colors; };
+  webappsFile = import ./webapps.nix;
+
+  webappTemplateScript =
+    { name, url }:
+    pkgs.writeShellScriptBin name ''
+      #!${pkgs.runtimeShell}
+      exec ${package.customBrave}/bin/brave --app=${url}
+    '';
+
+  # Generate a set of { name = pathToBinary; ... }
+  webappScripts = builtins.mapAttrs (
+    name: url: (webappTemplateScript { inherit name url; })
+  ) webappsFile.webapps;
 in
 {
-  programs.firejail.wrappedBinaries.brave = {
-    executable = "${package.customBrave}/bin/brave";
+  programs.firejail.wrappedBinaries = {
+    brave = {
+      executable = "${package.customBrave}/bin/brave";
+      profile = "${pkgs.firejail}/etc/firejail/brave.profile";
+      extraArgs = [
+        "--nonewprivs"
+        "--whitelist=/home/${vars.user.name}/.local/share/home.html"
+      ];
+    };
+  }
+  // builtins.mapAttrs (name: script: {
+    executable = "${script}/bin/${name}";
     profile = "${pkgs.firejail}/etc/firejail/brave.profile";
     extraArgs = [
       "--nonewprivs"
       "--whitelist=/home/${vars.user.name}/.local/share/home.html"
     ];
-  };
+  }) webappScripts;
 }
