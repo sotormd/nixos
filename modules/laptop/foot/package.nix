34b2263e2525e9950a5168ae3e3a24ded68ca00b
vrm <sotormd@proton.me>
2025-11-15 13:11:40 +0530

    feat(scripts): add disks

--- modules/laptop/foot/package.nix
+++ modules/laptop/foot/package.nix
@@ -1,4 +1,9 @@
-{ pkgs, colors, ... }:
+{
+  pkgs,
+  colors,
+  vars,
+  ...
+}:
 
 let
   config = import ./config.nix { inherit pkgs colors; };
@@ -7,6 +12,14 @@ in
   foot = pkgs.writeShellScriptBin "foot" ''
     #! ${pkgs.runtimeShell}
 
-    ${pkgs.foot}/bin/foot --config=${config.configDir}/foot.ini "$@"
+    FOCUSED_OUT="$(${pkgs.swayfx}/bin/swaymsg -t get_outputs -r | ${pkgs.jq}/bin/jq -r '.[] | select(.focused == true).name')"
+
+    if [ "$FOCUSED_OUT" = "${vars.outputs.laptop}" ]; then
+      SIZE=7
+    else
+      SIZE=10
+    fi
+
+    ${pkgs.foot}/bin/foot --config=${config.configDir}/foot.ini --font "${colors.fonts.monospace}:size=$SIZE" "$@"
   '';
 }
