3dabfbb73dd879f46d6ff4ac78dc1447666cbc01
vrm <sotormd@proton.me>
2025-11-25 12:03:37 +0530

    feat(images): modularize!

---
+++ modules/images/base.nix
@@ -0,0 +1,15 @@
+{
+  imports = [
+    # nix package manager configuration
+    ../../modules/common/nix
+
+    # list of packages
+    ./packages.nix
+  ];
+
+  time.timeZone = "UTC";
+
+  environment.sessionVariables.EDITOR = "vi";
+
+  nixpkgs.hostPlatform.system = "x86_64-linux";
+}
--- modules/images/gnome.nix
+++ modules/images/gnome.nix
@@ -1,12 +1,15 @@
-{ modulesPath, ... }:
+{ pkgs, modulesPath, ... }:
 
 {
   imports = [
+    # gnome installer cd module
     "${modulesPath}/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
 
-    ./minimal.nix
+    # base configuration
+    ./base.nix
   ];
 
+  environment.systemPackages = with pkgs; [ nerd-fonts.fira-code ];
   services.desktopManager.gnome.extraGSettingsOverrides = ''
     [org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles/:aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee/]
     use-system-font=false
