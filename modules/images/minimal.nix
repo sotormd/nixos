91a28cab56ae54dd91c7ef60b2f77e04f4ac528f
vrm <sotormd@proton.me>
2025-11-25 12:03:37 +0530

    feat(images): modularize!

--- modules/images/minimal.nix
+++ modules/images/minimal.nix
@@ -1,22 +1,11 @@
-{ pkgs, modulesPath, ... }:
+{ modulesPath, ... }:
 
 {
   imports = [
     # installation cd module
     "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
 
-    # nix package manager configuration
-    ../../modules/common/nix
-
-    # list of packages
-    ./packages.nix
+    # base configuration
+    ./base.nix
   ];
-
-  time.timeZone = "UTC";
-
-  environment.systemPackages = with pkgs; [ nerd-fonts.fira-code ];
-
-  environment.sessionVariables.EDITOR = "vi";
-
-  nixpkgs.hostPlatform.system = "x86_64-linux";
 }
