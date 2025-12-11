791da9c2d04312118a6dd59de646e1cb8f78fd4e
vrm <sotormd@proton.me>
2025-12-11 21:48:20 +0530

    feat(scripts): add all scripts to /nix/store

--- modules/common/scripts/bin.nix
+++ modules/common/scripts/bin.nix
@@ -1,8 +1,47 @@
 { pkgs, vars, ... }:
 
 let
-  nixosScript = pkgs.writeShellScriptBin "nixos" (builtins.readFile ../../../scripts/nixos);
+  scriptNames = [
+    "test"
+    "switch"
+    "commit"
+    "update"
+    "perms"
+    "format"
+    "repair"
+    "serverpush"
+    "help"
+    "init"
+  ];
+
+  mkScript =
+    name:
+    pkgs.writeTextFile {
+      inherit name;
+      text = builtins.readFile ../../../scripts/${name};
+      destination = "/${name}";
+      executable = true;
+    };
+
+  scripts = map mkScript scriptNames;
+
+  scriptsDir = pkgs.symlinkJoin {
+    name = "scripts";
+    paths = scripts;
+  };
+
+  nixosScriptRaw = pkgs.writeShellScriptBin "nixos" (builtins.readFile ../../../scripts/nixos);
+
+  nixosWrapper = pkgs.writeShellScriptBin "nixos" ''
+    #! ${pkgs.runtimeShell}
+
+    export NIXOS_SCRIPTS_DIR=${scriptsDir}
+
+    ${nixosScriptRaw}/bin/nixos "$@"
+  '';
 in
 {
-  users.users.${vars.user.name}.packages = [ nixosScript ];
+  users.users.${vars.user.name}.packages = [
+    nixosWrapper
+  ];
 }
