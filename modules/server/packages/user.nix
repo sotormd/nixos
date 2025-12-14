5d9083a97e220eb28c0f379d524a76d515e47758
vrm <sotormd@proton.me>
2025-12-14 14:35:59 +0530

    feat(server): add vim

--- modules/server/packages/user.nix
+++ modules/server/packages/user.nix
@@ -2,5 +2,10 @@
 
 {
   # set of packages to appear in user environment
-  users.users.${vars.user.name}.packages = with pkgs; [ ];
+  users.users.${vars.user.name}.packages = with pkgs; [
+    # vim - vi improved
+    vim
+  ];
+
+  environment.sessionVariables.EDITOR = "vi";
 }
