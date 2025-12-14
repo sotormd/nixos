a175149e726c6347351bd886c763216aab8135e3
vrm <sotormd@proton.me>
2025-12-14 15:57:30 +0530

    feat(common): add service to set ownership of /persist/nixos

--- modules/common/users/main.nix
+++ modules/common/users/main.nix
@@ -10,13 +10,9 @@
 
     home = "/home/${vars.user.name}";
     createHome = true;
-
-    uid = 1000;
   };
 
   users.groups = {
-    "${vars.user.name}" = {
-      gid = 1000;
-    };
+    "${vars.user.name}" = { };
   };
 }
