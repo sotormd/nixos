5918062529990ed6daf1d7944d177463bbf22301
vrm <sotormd@proton.me>
2025-12-21 11:41:16 +0530

    feat(laptop): disable gnome tour in nomad mode

--- modules/laptop/nomad/gnome.nix
+++ modules/laptop/nomad/gnome.nix
@@ -1,4 +1,9 @@
 {
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
+
+  services.desktopManager.gnome.extraGSettingsOverrides = ''
+    [org.gnome.shell]
+    welcome-dialog-last-shown-version='9999999999'
+  '';
 }
