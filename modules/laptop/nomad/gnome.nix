5f5546d513676e0c92bff522c3b74793931c0129
vrm <sotormd@proton.me>
2025-12-22 09:48:10 +0530

    feat(laptop): enable gnome-software and games in nomad mode

--- modules/laptop/nomad/gnome.nix
+++ modules/laptop/nomad/gnome.nix
@@ -2,6 +2,9 @@
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 
+  services.gnome.gnome-software.enable = true;
+  services.gnome.games.enable = true;
+
   services.desktopManager.gnome.extraGSettingsOverrides = ''
     [org.gnome.shell]
     welcome-dialog-last-shown-version='9999999999'
