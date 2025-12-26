0797c2deb3c07ed355c8a35c50a38d07fa37c28f
vrm <sotormd@proton.me>
2025-12-26 11:32:38 +0530

    feat(laptop): improve firejail for i2p-browser

--- modules/laptop/i2p-browser/firejail.nix
+++ modules/laptop/i2p-browser/firejail.nix
@@ -12,6 +12,28 @@ in
   programs.firejail.wrappedBinaries.i2p-browser = {
     executable = "${package.i2pBrowser}/bin/i2p-browser";
     profile = "${pkgs.firejail}/etc/firejail/firefox.profile";
-    extraArgs = [ "--nonewprivs" ];
+    extraArgs = [
+      "--nonewprivs"
+
+      "--caps.drop=all"
+
+      "--no3d"
+      "--nodbus"
+      "--nodvd"
+      "--nogroups"
+      "--noprinters"
+      "--noroot"
+      "--nosound"
+      "--nou2f"
+      "--novideo"
+
+      "--private-cache"
+      "--private-cwd"
+      "--private-dev"
+      "--private-etc"
+      "--private-tmp"
+
+      "--seccomp"
+    ];
   };
 }
