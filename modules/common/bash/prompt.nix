3719ca35d011a1f31ce7025eb2d6808d2f4063fb
vrm <sotormd@proton.me>
2026-01-31 14:34:02 +0530

    feat(common): new prompt

--- modules/common/bash/prompt.nix
+++ modules/common/bash/prompt.nix
@@ -2,32 +2,31 @@
 
 {
   # cool bash prompt
-  # looks like this:
-  #
-  # nixos ~ λ
-  #
-  # but in green
+  # green for user, red for root
   programs.bash.promptInit = lib.mkDefault ''
     # Only set prompt if terminal supports it
     if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
-      # Set color based on UID (root = red, others = green)
+
+      # Color based on UID (root = red, others = green)
       if [ "$EUID" -eq 0 ]; then
-        PROMPT_COLOR="1;31m"  # bold red
+        PROMPT_COLOR="1;31m"
       else
-        PROMPT_COLOR="1;32m"  # bold green
+        PROMPT_COLOR="1;32m"
       fi
 
-      if [ -n "$INSIDE_EMACS" ]; then
-        # Emacs terminal mode, no title escape sequences
-        PS1="\n\[\033[$PROMPT_COLOR\]\h \w λ\[\033[0m\] "
-      else
-        # Normal terminal, include xterm title
-        PS1="\n\[\033[$PROMPT_COLOR\]\[\e]0;\u@\h: \w\a\]\h \w λ\[\033[0m\] "
-      fi
+      # ---- foot-specific prompt ----
+      if [ "$TERM" = "foot" ]; then
+        # [noir:/directory] λ
+        PS1="\n\[\033[$PROMPT_COLOR\][noir:\w] λ\[\033[0m\] "
 
-      # xterm-specific additional title sequence
-      if [ "$TERM" = "xterm" ]; then
-        PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+      else
+        # ---- all other terminals ----
+        PS1="\n\[\033[$PROMPT_COLOR\][noir:\w] $\[\033[0m\] "
+
+        # xterm extra title
+        if [ "$TERM" = "xterm" ]; then
+          PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+        fi
       fi
     fi
   '';
