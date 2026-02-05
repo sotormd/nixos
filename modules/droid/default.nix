4a0d788354399f10e2223bbef4e3b255b0d91e21
vrm <sotormd@proton.me>
2026-02-05 15:29:56 +0530

    feat(droid): update bash prompt

--- modules/droid/default.nix
+++ modules/droid/default.nix
@@ -25,7 +25,7 @@
 
   environment.sessionVariables = {
     NIXOS_ROLE = "droid";
-    PS1 = ''\n\[\033[1;32m\]nix \w λ\[\033[0m\] '';
+    PS1 = ''\n\[\033[1;32m\][\w] λ\[\033[0m\] '';
   };
 
   environment.motd = ''
