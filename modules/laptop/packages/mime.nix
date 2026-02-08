7f6daea21d97075cffc8b5c8251e4c4f2717c5a5
vrm <sotormd@proton.me>
2026-02-08 18:34:50 +0530

    fix(laptop): xdg mimetypes

--- modules/laptop/packages/mime.nix
+++ modules/laptop/packages/mime.nix
@@ -1,5 +1,3 @@
-{ config, ... }:
-
 {
   xdg.mime.enable = true;
 
@@ -21,16 +19,16 @@
 
     "application/pdf" = "org.pwmt.zathura.desktop";
 
-    "text/plain" = "mousepad.desktop";
-    "text/x-shellscript" = "mousepad.desktop";
-    "text/x-python" = "mousepad.desktop";
-    "text/x-rustsrc" = "mousepad.desktop";
-    "text/x-nix" = "mousepad.desktop";
-    "text/x-c" = "mousepad.desktop";
-    "text/markdown" = "mousepad.desktop";
-    "application/json" = "mousepad.desktop";
-    "application/x-yaml" = "mousepad.desktop";
-    "application/xml" = "mousepad.desktop";
+    "text/plain" = "org.xfce.mousepad.desktop";
+    "text/x-shellscript" = "org.xfce.mousepad.desktop";
+    "text/x-python" = "org.xfce.mousepad.desktop";
+    "text/x-rustsrc" = "org.xfce.mousepad.desktop";
+    "text/x-nix" = "org.xfce.mousepad.desktop";
+    "text/x-c" = "org.xfce.mousepad.desktop";
+    "text/markdown" = "org.xfce.mousepad.desktop";
+    "application/json" = "org.xfce.mousepad.desktop";
+    "application/x-yaml" = "org.xfce.mousepad.desktop";
+    "application/xml" = "org.xfce.mousepad.desktop";
 
     "text/html" = "brave-browser.desktop";
     "x-scheme-handler/http" = "brave-browser.desktop";
@@ -41,48 +39,5 @@
     "application/gzip" = "org.gnome.FileRoller.desktop";
     "application/x-tar" = "org.gnome.FileRoller.desktop";
     "application/x-7z-compressed" = "org.gnome.FileRoller.desktop";
-  };
-
-  hjem.users.${config.vars.user.name}.files = {
-    ".local/share/applications/mimeapps.list".text = ''
-      [Default Applications]
-      image/png=swayimg.desktop
-      image/jpeg=swayimg.desktop
-      image/webp=swayimg.desktop
-      image/gif=swayimg.desktop
-
-      audio/mpeg=mpv.desktop
-      audio/ogg=mpv.desktop
-      audio/flac=mpv.desktop
-      audio/wav=mpv.desktop
-
-      video/mp4=mpv.desktop
-      video/x-matroska=mpv.desktop
-      video/webm=mpv.desktop
-      video/ogg=mpv.desktop
-
-      application/pdf=org.pwmt.zathura.desktop
-
-      text/plain=mousepad.desktop
-      text/x-shellscript=mousepad.desktop
-      text/x-python=mousepad.desktop
-      text/x-rustsrc=mousepad.desktop
-      text/x-nix=mousepad.desktop
-      text/x-c=mousepad.desktop
-      text/markdown=mousepad.desktop
-      application/json=mousepad.desktop
-      application/x-yaml=mousepad.desktop
-      application/xml=mousepad.desktop
-
-      text/html=brave-browser.desktop
-      x-scheme-handler/http=brave-browser.desktop
-      x-scheme-handler/https=brave-browser.desktop
-      image/svg+xml=brave-browser.desktop
-
-      application/zip=org.gnome.FileRoller.desktop
-      application/gzip=org.gnome.FileRoller.desktop
-      application/x-tar=org.gnome.FileRoller.desktop
-      application/x-7z-compressed=org.gnome.FileRoller.desktop
-    '';
   };
 }
