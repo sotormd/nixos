06a91aaab4c9d8f7191fba42274e34a5b8d4f852
vrm <sotormd@proton.me>
2026-02-26 22:02:34 +0530

    feat(flake): add devshell

--- flake.nix
+++ flake.nix
@@ -69,16 +69,49 @@
 
       perSystem =
         { pkgs, ... }:
+        let
+          nixos = import ./modules/common/scripts/bin.nix { inherit pkgs; };
+        in
         {
           # formatter
           formatter = pkgs.nixfmt;
 
           # scripts as a package
-          packages.default =
-            let
-              nixos = import ./modules/common/scripts/bin.nix { inherit pkgs; };
-            in
-            nixos.nixosWrapper;
+          packages.default = nixos.nixosWrapper;
+
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.lix
+              pkgs.nixos-rebuild-ng
+              pkgs.nixos-install-tools
+
+              pkgs.cryptsetup
+              pkgs.zfs
+              pkgs.dosfstools
+              pkgs.util-linux
+              pkgs.udev
+
+              pkgs.bash
+              pkgs.coreutils-full
+              pkgs.gawk
+              pkgs.gnugrep
+              pkgs.sops
+              pkgs.gnupg
+              pkgs.yq
+              pkgs.mkpasswd
+              pkgs.git
+
+              pkgs.tree
+              pkgs.rsync
+              pkgs.openssh
+              pkgs.sbctl
+              pkgs.nixfmt
+
+              pkgs.systemd
+
+              nixos.nixosWrapper
+            ];
+          };
         };
 
       flake =
