fb445e165f5222c00d8a2794b0dfa055d8637a07
vrm <sotormd@proton.me>
2026-03-03 00:19:11 +0530

    feat(common): extend usbguard rules

--- modules/machines/common/usbguard/settings.nix
+++ modules/machines/common/usbguard/settings.nix
@@ -7,6 +7,15 @@
 
     # allow devices that are already connected when the daemon starts
     presentDevicePolicy = "allow";
+
+    # block suspicious devices
+    # https://docs.redhat.com/en/documentation/red_hat_enterprise_linux/7/html/security_guide/sec-using-usbguard
+    rules = ''
+      reject with-interface all-of { 08:*:* 03:00:* }
+      reject with-interface all-of { 08:*:* 03:01:* }
+      reject with-interface all-of { 08:*:* e0:*:* }
+      reject with-interface all-of { 08:*:* 02:*:* }
+    '';
   };
 
   environment.systemPackages = [ pkgs.usbguard ];
