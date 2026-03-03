764006a3ac96ae1310aad301fd59a854604789ee
vrm <sotormd@proton.me>
2026-03-03 15:19:03 +0530

    refactor: remove gnome software and games
    laptop: nomad mode no longer uses gnome software and games

--- modules/machines/laptop/nomad/gnome.nix
+++ modules/machines/laptop/nomad/gnome.nix
@@ -2,9 +2,6 @@
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 
-  services.gnome.gnome-software.enable = true;
-  services.gnome.games.enable = true;
-
   services.desktopManager.gnome.extraGSettingsOverrides = ''
     [org.gnome.shell]
     welcome-dialog-last-shown-version='9999999999'
