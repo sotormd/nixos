ec06f205576ec910d899bb317486b20dd83d677f
vrm <sotormd@proton.me>
2026-03-06 09:00:24 +0530

    feat: begin transition from sudo to run0

---
+++ modules/machines/common/sudo/run0.nix
@@ -0,0 +1,9 @@
+{ config, pkgs, ... }:
+
+{
+  users.users.${config.vars.user.name}.packages = [
+    (pkgs.writeShellScriptBin "run0" ''
+      /run/current-system/sw/bin/run0 --background="" "$@"
+    '')
+  ];
+}
