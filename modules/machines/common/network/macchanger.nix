d31a582dce61325755d358cdbe23fd2de28d6a96
vrm <sotormd@proton.me>
2026-03-07 15:24:29 +0530

    fix: network issues on server
    - networking dies after reactivation
    - timesyncd fails to sync

--- modules/machines/common/network/macchanger.nix
+++ modules/machines/common/network/macchanger.nix
@@ -7,8 +7,8 @@ in
   systemd.services.macchanger = {
     enable = true;
     description = "GNU MAC Changer for ${iface}";
+    requiredBy = [ "wpa_supplicant-${config.vars.network.interface}.service" ];
     before = [ "network-setup.service" ];
-    wantedBy = [ "multi-user.target" ];
     path = [ pkgs.macchanger ];
     serviceConfig.Type = "oneshot";
     script = ''
--- modules/machines/laptop/network/dns.nix
+++ modules/machines/laptop/network/dns.nix
@@ -1,11 +1,14 @@
 { config, lib, ... }:
 
-{
-  networking.nameservers = lib.concatMap (x: x) [
-    (lib.optional config.vars.features.selfhosted.enable config.vars.network.server.address)
-    [
-      "1.1.1.1"
-      "1.0.0.1"
+let
+  text = lib.concatStringsSep "\n" (
+    lib.concatMap (x: x) [
+      (lib.optional config.vars.features.selfhosted.enable "nameserver ${config.vars.network.server.address}")
+      [ "nameserver 1.1.1.1" ]
+      [ "nameserver 1.0.0.1" ]
     ]
-  ];
+  );
+in
+{
+  environment.etc."resolv.conf".text = text;
 }
