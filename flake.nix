cf948aaeb0253ca1cb48a6aea28b0711f5166121
vrm <sotormd@proton.me>
2026-03-14 11:59:01 +0530

    docs: improve comments in flake.nix

--- flake.nix
+++ flake.nix
@@ -2,76 +2,102 @@
   description = "nixos configuration flake";
 
   inputs = {
+
+    # the Nix package repository
+    # we track the nixos-unstable
+    # branch on every machine
     nixpkgs = {
       url = "github:nixos/nixpkgs/nixos-unstable";
     };
 
+    # to deal with system
+    # without writing a lambda
+    # tbh writing a lambda is probably simpler
+    # but whatever
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
     };
 
+    # simple symlinking to $HOME
+    # because some apps dont fw wrappers
+    # used only on laptop
     hjem = {
       url = "github:feel-co/hjem";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # secrets for nixos
+    # because we dont want them ending
+    # up in the world-readable Nix store
     sops-nix = {
       url = "github:Mic92/sops-nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # secure boot for nixos
+    # used only on laptop
     lanzaboote = {
       url = "github:nix-community/lanzaboote";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # nix-on-droid breaks on unstable
+    # pin to last working commit instead
+    # https://github.com/nix-community/nix-on-droid/issues/495
     nixpkgs-droid = {
-      # nix-on-droid breaks on unstable
-      # pin to last working commit instead
-      # https://github.com/nix-community/nix-on-droid/issues/495
       url = "github:nixos/nixpkgs/88d3861";
     };
 
+    # Nix package manager for android
     nix-on-droid = {
       url = "github:nix-community/nix-on-droid/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs-droid";
     };
 
+    # StevenBlack's host lists
+    # because we dont want PiHole
     hosts = {
       url = "github:StevenBlack/hosts";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # my Neovim configuration
     neovim = {
       url = "github:sotormd/neovim";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.colors.follows = "colors";
     };
 
+    # my colors & theming
     colors = {
       url = "github:sotormd/colors";
     };
 
+    # my wallpapers
     wallpapers = {
       url = "github:sotormd/wallpapers";
     };
 
+    # XKCD comics on my lockscreen
     xkcd = {
       url = "github:sotormd/xkcd-wall";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    # my browser homepage
     homepage = {
       url = "github:sotormd/homepage";
     };
+
   };
 
   outputs =
     { self, ... }@inputs:
     inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+
       systems = [
-        "x86_64-linux"
-        "aarch64-linux"
+        "x86_64-linux" # machine-laptop, image-gnome, image-minimal
+        "aarch64-linux" # machine-server, image-sd, image-sd-remote, droid
       ];
 
       perSystem =
@@ -80,22 +106,33 @@
           nixos = import ./modules/machines/common/cli/bin.nix { inherit pkgs; };
         in
         {
+
           # formatter
+          # nixfmt-tree can take an entire directory
           formatter = pkgs.nixfmt-tree;
 
           # cli devshell
+          # containes everything for the cli to work
+          # not documented, but this can be used instead of
+          # the included images for installation
           devShells.default = pkgs.mkShell {
             packages = import ./modules/images/bootstrapPackages.nix { inherit pkgs; } ++ [
               nixos.nixosWrapper
             ];
           };
+
         };
 
       flake =
         let
+
+          # additional lib functions
           lib = inputs.nixpkgs.lib // (import ./lib);
+
+          # the old variables interface, now using options!
           legacyVars = import ./vars/vars.nix;
 
+          # create a "host"
           mkHost =
             {
               role,
@@ -114,6 +151,7 @@
               modules = [ (import ./hosts { inherit role; }) ];
             };
 
+          # create a "machine"
           mkMachine =
             role: system:
             mkHost {
@@ -122,6 +160,7 @@
               withVars = true;
             };
 
+          # create an "image"
           mkImage =
             role: system:
             mkHost {
@@ -129,8 +168,10 @@
               inherit system;
               withVars = false;
             };
+
         in
         {
+
           nixosConfigurations = {
 
             # machines
@@ -151,6 +192,7 @@
             pkgs = import inputs.nixpkgs-droid { system = "aarch64-linux"; };
             modules = [ (import ./hosts { role = "droid"; }) ];
           };
+
         };
     };
 }
