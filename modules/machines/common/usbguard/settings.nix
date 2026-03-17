57574f8160a3e2a5529e192e94be60ea2af2ab7d
vrm <sotormd@proton.me>
2026-03-18 01:40:43 +0530

    feat: stricter usbguard rules

--- modules/machines/common/usbguard/settings.nix
+++ modules/machines/common/usbguard/settings.nix
@@ -1,12 +1,20 @@
-{ pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
+let
+  allowText = lib.concatStringsSep "\n" (map (x: "allow ${x}") config.vars.usbs);
+in
 {
   services.usbguard = {
     # only allow root
     IPCAllowedUsers = [ "root" ];
 
-    # allow devices that are already connected when the daemon starts
-    presentDevicePolicy = "allow";
+    # allow only declared devices
+    presentDevicePolicy = "apply-policy";
 
     # block suspicious devices
     # https://docs.redhat.com/en/documentation/red_hat_enterprise_linux/7/html/security_guide/sec-using-usbguard
@@ -15,6 +23,7 @@
       reject with-interface all-of { 08:*:* 03:01:* }
       reject with-interface all-of { 08:*:* e0:*:* }
       reject with-interface all-of { 08:*:* 02:*:* }
+      ${allowText}
     '';
   };
 
