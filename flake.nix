9236dbdfcafff2f0643dd72c3fd6dc2f8a68a432
vrm <sotormd@proton.me>
2026-04-08 00:17:56 +0530

    refactor: separate droid

--- flake.nix
+++ flake.nix
@@ -41,19 +41,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    # nix-on-droid breaks on unstable
-    # pin to last working commit instead
-    # https://github.com/nix-community/nix-on-droid/issues/495
-    nixpkgs-droid = {
-      url = "github:nixos/nixpkgs/88d3861";
-    };
-
-    # Nix package manager for android
-    nix-on-droid = {
-      url = "github:nix-community/nix-on-droid/release-24.05";
-      inputs.nixpkgs.follows = "nixpkgs-droid";
-    };
-
     # StevenBlack's host lists
     # because we dont want PiHole
     hosts = {
@@ -102,7 +89,7 @@
 
       systems = [
         "x86_64-linux" # machine-laptop, image-gnome, image-minimal
-        "aarch64-linux" # machine-server, image-sd, image-sd-remote, droid
+        "aarch64-linux" # machine-server, image-sd, image-sd-remote
       ];
 
       perSystem =
@@ -190,13 +177,6 @@
             image-sd = mkImage "sd" "aarch64-linux";
             image-sd-remote = mkImage "sd-remote" "aarch64-linux";
 
-          };
-
-          # nix-on-droid
-          nixOnDroidConfigurations.default = inputs.nix-on-droid.lib.nixOnDroidConfiguration {
-            extraSpecialArgs = { inherit inputs; };
-            pkgs = import inputs.nixpkgs-droid { system = "aarch64-linux"; };
-            modules = [ (import ./hosts { role = "droid"; }) ];
           };
 
         };
