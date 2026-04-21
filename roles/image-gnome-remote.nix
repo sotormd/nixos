0cb01839cf3ca993088fcba509024ae015588649
vrm <sotormd@proton.me>
2026-04-21 14:08:43 +0530

    refactor: cleanup

---
+++ roles/image-gnome-remote.nix
@@ -0,0 +1,8 @@
+{
+  imports = [
+    ../modules/common
+    ../modules/images/common
+    ../modules/images/gnome.nix
+    ../modules/images/compose/remote.nix
+  ];
+}
---
+++ roles/image-minimal-remote.nix
@@ -0,0 +1,8 @@
+{
+  imports = [
+    ../modules/common
+    ../modules/images/common
+    ../modules/images/minimal.nix
+    ../modules/images/compose/remote.nix
+  ];
+}
