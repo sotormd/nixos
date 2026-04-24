a0ee8ea04b5593263ec87c65f2320869b283919b
vrm <sotormd@proton.me>
2026-04-24 18:22:22 +0530

    fix: xdg-dbus-proxy doesn't register MPRIS information when sway is started with dbus-run-session

--- modules/sway/swaywm/start.nix
+++ modules/sway/swaywm/start.nix
@@ -8,7 +8,7 @@
          && [ -n "$XDG_VTNR" ] \
          && [ "$XDG_VTNR" -eq 1 ] \
          && [ "$USER" = "${config.vars.user.name}" ]; then
-        exec dbus-run-session ${pkgs.sway0}/bin/sway
+        exec ${pkgs.sway0}/bin/sway
       fi
     '';
   };
