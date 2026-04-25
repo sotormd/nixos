56df560fbf9722dc319298929d0a5b3c3d6bfadb
vrm <sotormd@proton.me>
2026-04-25 11:50:21 +0530

    refactor: rename profiles

--- roles/machine-server/default.nix
+++ roles/machine-server/default.nix
@@ -7,8 +7,8 @@
     inputs.hosts.nixosModule
     inputs.sops-nix.nixosModules.sops
     inputs.self.nixosModules.profiles.machine
-    inputs.self.nixosModules.profiles.pi
     inputs.self.nixosModules.profiles.selfhost
+    inputs.self.nixosModules.profiles.raspi
   ];
 
   # populate the variables
