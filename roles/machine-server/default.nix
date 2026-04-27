6fa18420c054aff961e73c7370c5df5a4beefb28
vrm <sotormd@proton.me>
2026-04-27 20:31:04 +0530

    refactor: cleanup

--- roles/machine-server/default.nix
+++ roles/machine-server/default.nix
@@ -7,8 +7,8 @@
     inputs.hosts.nixosModule
     inputs.sops-nix.nixosModules.sops
     inputs.self.nixosModules.profiles.machine
-    inputs.self.nixosModules.profiles.selfhost
     inputs.self.nixosModules.profiles.raspi
+    inputs.self.nixosModules.profiles.selfhost
   ];
 
   # populate the variables
