8821bc7af049053345ee7398ae2f84b03c57bfb6
vrm <sotormd@proton.me>
2026-05-01 18:20:28 +0530

    feat: save previous lockscreen before xkcd-refresh

--- modules/sway/swaylock/xkcd.nix
+++ modules/sway/swaylock/xkcd.nix
@@ -18,14 +18,15 @@ let
   };
 
   fallback = config.wallpapers.oc.nixos;
-
   target = "/home/${config.vars.user.name}/.local/share/xkcd.png";
+  backup = "/home/${config.vars.user.name}/.local/share/xkcd.last.png";
 
   xkcdWrapped = pkgs.writeTextFile {
     name = "xkcd-wrapped";
     text = ''
       #!${pkgs.runtimeShell}
 
+      ${pkgs.coreutils}/bin/cp -f "${target}" "${backup}" 2>/dev/null || ${pkgs.coreutils}/bin/true
       ${inputs.xkcd.packages.x86_64-linux.default}/bin/xkcd-wall -t random -c "${configuration}/config.json" "${target}" || cp "${fallback}" "${target}"; chmod 777 "${target}"
     '';
     destination = "/bin/xkcd-refresh";
