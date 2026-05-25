c1ea26f8cec4e765917eb73b29b25425dac04a91
sotormd <sotormd@proton.me>
2026-05-25 11:09:50 +0530

    feat: update bash prompt

--- modules/apps/bash/prompt.nix
+++ modules/apps/bash/prompt.nix
@@ -2,28 +2,22 @@
 
 {
   # cool bash prompt
-  # green for user, red for root
   programs.bash.promptInit = lib.mkDefault ''
-    # Only set prompt if terminal supports it
     if [ "$TERM" != "dumb" ] || [ -n "$INSIDE_EMACS" ]; then
 
-      # Color based on UID (root = red, others = green)
       if [ "$EUID" -eq 0 ]; then
         PROMPT_COLOR="1;31m"
+        PROMPT_SYMBOL="#"
       else
         PROMPT_COLOR="1;32m"
-      fi
-
-      if [ "$TERM" != "linux" ]; then
-        PS1="\n\[\033[$PROMPT_COLOR\][\h:\w] λ\[\033[0m\] "
-
-      else
-        PS1="\n\[\033[$PROMPT_COLOR\][\h:\w] $\[\033[0m\] "
-
-        if [ "$TERM" = "xterm" ]; then
-          PS1="\[\033]2;\h:\u:\w\007\]$PS1"
+        if [ "$TERM" != "linux" ]; then
+          PROMPT_SYMBOL="λ"
+        else
+          PROMPT_SYMBOL='$'
         fi
       fi
+
+      PS1="\n\[\033[$PROMPT_COLOR\][\h:\w] $PROMPT_SYMBOL\[\033[0m\] "
     fi
   '';
 }
