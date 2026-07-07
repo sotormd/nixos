f2f1173ba93b4c1710c1502af1b013eae11d35f7
sotormd <sotormd@proton.me>
2026-07-07 20:30:33 +0530

    fix: prevent sshd from dying due to restart limits

--- modules/services/sshd/online.nix
+++ modules/services/sshd/online.nix
@@ -8,6 +8,7 @@ lib.mkIf ssh.enable {
   systemd.services.sshd = {
     wants = [ "network-online.target" ];
     after = [ "network-online.target" ];
+    unitConfig.StartLimitIntervalSec = 0;
   };
 
 }
