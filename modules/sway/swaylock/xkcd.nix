86504ac8679b8ef09cd1246f6164bbaae56493da
sotormd <sotormd@proton.me>
2026-07-21 20:32:30 +0530

    chore: update lockfile

--- modules/sway/swaylock/xkcd.nix
+++ modules/sway/swaylock/xkcd.nix
@@ -10,17 +10,6 @@
 }:
 
 let
-  configuration = writeTextFile {
-    name = "xkcd-config";
-    text = builtins.toJSON {
-      background-colors = colors.xkcd.bgs;
-      foreground-colors = colors.xkcd.fgs;
-      dimensions = vars.displays.outputs.${vars.displays.primary}.resolution;
-    };
-    destination = "/config.json";
-    executable = false;
-  };
-
   fallback = wallpapers.nord.nixos;
   target = "/home/${vars.user.name}/.local/share/xkcd.png";
   backup = "/home/${vars.user.name}/.local/share/xkcd.last.png";
@@ -31,7 +20,12 @@ let
       #!${runtimeShell}
 
       ${coreutils}/bin/cp -f "${target}" "${backup}" 2>/dev/null || ${coreutils}/bin/true
-      ${inputs.xkcd.packages.x86_64-linux.default}/bin/xkcd-wall -t random -c "${configuration}/config.json" "${target}" || cp "${fallback}" "${target}"; chmod 777 "${target}"
+      ${inputs.xkcd.packages.x86_64-linux.default}/bin/xkcd-wall \
+        -t random \
+        -d ${vars.displays.outputs.${vars.displays.primary}.resolution} \
+        -b ${colors.xkcd.bg} \
+        -f ${colors.xkcd.fg} \
+        "${target}" || cp "${fallback}" "${target}"; chmod 777 "${target}"
     '';
     destination = "/bin/xkcd-refresh";
     executable = true;
