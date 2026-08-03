282b07da100039d6cec13df6ffa95f289efe13d2
sotormd <sotormd@proton.me>
2026-08-03 16:57:03 +0530

    refactor: simplify flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NixOS configuration for multiple hosts, with custom Impermanence, MicroVMs, and CLI tooling. Plus ZFS, WireGuard, bootstrap images, etc.";
+  description = "NixOS configuration for multiple hosts.";
 
   inputs = {
 
