6b50adedaebed91b96310e77db8036647ab7acdf
sotormd <sotormd@proton.me>
2026-09-17 00:00:43 +0530

    fix: i2p-browser jail

--- modules/apps/i2p-browser/bubblewrap.nix
+++ modules/apps/i2p-browser/bubblewrap.nix
@@ -2,7 +2,7 @@
   lib,
   bubblewrap,
   coreutils,
-  writeShellScriptBin,
+  writeTextFile,
   script,
   vars,
 }:
@@ -11,46 +11,51 @@ let
   user = vars.user.name;
   home = "/home/${user}";
 
-  jail = writeShellScriptBin "i2p-browser-jail" ''
-    set -euo pipefail
+  jail = writeTextFile {
+    name = "i2p-browser-jail";
+    text = ''
+      set -euo pipefail
 
-    mkdir -p "$XDG_RUNTIME_DIR/bubblewrap-i2p-browser"
+      ${lib.getExe' coreutils "mkdir"} -p "$XDG_RUNTIME_DIR/bubblewrap-i2p-browser"
 
-    users=$(${lib.getExe' coreutils "mktemp"} -d -p "$XDG_RUNTIME_DIR/bubblewrap-i2p-browser" users.XXXXXX)
-    echo "i2p-browser:x:1000:1000:i2p-browser:/home/i2p-browser:${lib.getExe' coreutils "false"}" > "$users/passwd"
-    echo "i2p-browser:x:1000:" > "$users/group"
+      users=$(${lib.getExe' coreutils "mktemp"} -d -p "$XDG_RUNTIME_DIR/bubblewrap-i2p-browser" users.XXXXXX)
+      echo "i2p-browser:x:1000:1000:i2p-browser:/home/i2p-browser:${lib.getExe' coreutils "false"}" > "$users/passwd"
+      echo "i2p-browser:x:1000:" > "$users/group"
 
-    cleanup() { 
-      rm -rf "$XDG_RUNTIME_DIR/bubblewrap-i2p-browser" 
-    }
-    trap cleanup INT TERM EXIT
+      cleanup() { 
+        ${lib.getExe' coreutils "rm"} -rf "$XDG_RUNTIME_DIR/bubblewrap-i2p-browser" 
+      }
+      trap cleanup INT TERM EXIT
 
-    ${lib.getExe bubblewrap} \
-      --ro-bind /nix/store /nix/store \
-      --ro-bind "$XDG_RUNTIME_DIR/wayland-1" "$XDG_RUNTIME_DIR/wayland-1" \
-      --ro-bind "$users/passwd" /etc/passwd \
-      --ro-bind "$users/group" /etc/group \
-      --ro-bind /etc/resolv.conf /etc/resolv.conf \
-      --ro-bind /etc/fonts /etc/fonts \
-      --tmpfs /tmp \
-      --setenv HOME /home/i2p-browser \
-      --tmpfs /home/i2p-browser \
-      --ro-bind ${home}/.gtkrc-2.0 /home/i2p-browser/.gtkrc-2.0 \
-      --ro-bind ${home}/.config/gtk-3.0 /home/i2p-browser/.config/gtk-3.0 \
-      --ro-bind ${home}/.config/gtk-4.0 /home/i2p-browser/.config/gtk-4.0 \
-      --ro-bind ${home}/.icons /home/i2p-browser/.icons \
-      --ro-bind ${home}/.Xresources /home/i2p-browser/.Xresources \
-      --ro-bind ${home}/.local/share/fonts /home/i2p-browser/.local/share/fonts \
-      --ro-bind ${home}/.local/share/icons /home/i2p-browser/.local/share/icons \
-      --ro-bind ${home}/.local/share/themes /home/i2p-browser/.local/share/themes \
-      --ro-bind ${home}/.config/dconf /home/i2p-browser/.config/dconf \
-      --proc /proc \
-      --dev /dev  \
-      --unshare-all \
-      --share-net \
-      --die-with-parent \
-      --new-session \
-      ${lib.getExe script} "$@"
-  '';
+      ${lib.getExe bubblewrap} \
+        --ro-bind /nix/store /nix/store \
+        --ro-bind "$XDG_RUNTIME_DIR/wayland-1" "$XDG_RUNTIME_DIR/wayland-1" \
+        --ro-bind "$users/passwd" /etc/passwd \
+        --ro-bind "$users/group" /etc/group \
+        --ro-bind /etc/resolv.conf /etc/resolv.conf \
+        --ro-bind /etc/fonts /etc/fonts \
+        --tmpfs /tmp \
+        --setenv HOME /home/i2p-browser \
+        --tmpfs /home/i2p-browser \
+        --ro-bind ${home}/.gtkrc-2.0 /home/i2p-browser/.gtkrc-2.0 \
+        --ro-bind ${home}/.config/gtk-3.0 /home/i2p-browser/.config/gtk-3.0 \
+        --ro-bind ${home}/.config/gtk-4.0 /home/i2p-browser/.config/gtk-4.0 \
+        --ro-bind ${home}/.icons /home/i2p-browser/.icons \
+        --ro-bind ${home}/.Xresources /home/i2p-browser/.Xresources \
+        --ro-bind ${home}/.local/share/fonts /home/i2p-browser/.local/share/fonts \
+        --ro-bind ${home}/.local/share/icons /home/i2p-browser/.local/share/icons \
+        --ro-bind ${home}/.local/share/themes /home/i2p-browser/.local/share/themes \
+        --ro-bind ${home}/.config/dconf /home/i2p-browser/.config/dconf \
+        --proc /proc \
+        --dev /dev  \
+        --unshare-all \
+        --share-net \
+        --die-with-parent \
+        --new-session \
+        ${lib.getExe script} "$@"
+    '';
+    destination = "/bin/i2p-browser";
+    executable = true;
+  };
 in
 jail
