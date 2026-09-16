b91c70fa259fe868b8c29d712e43ec48596ea712
sotormd <sotormd@proton.me>
2026-09-17 00:01:01 +0530

    fix: rofi configuration

--- modules/sway/rofi/package.nix
+++ modules/sway/rofi/package.nix
@@ -9,7 +9,7 @@ let
   name = "rofi";
   base = rofi;
   command = ''
-    ${lib.getExe base} -config ${configuration}/config.rasi "$@"
+    ${lib.getExe base} -config ${configuration} "$@"
   '';
 
   rofiWrapped = callPackage lib.mkWrapperPackage { inherit name base command; };
